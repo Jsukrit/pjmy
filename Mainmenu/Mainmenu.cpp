02afa1a9488a48cb6f3cec0af70510e747091c79
Nice <niceziio08@gmail.com>
2025-01-26 21:03:00 +0700

    หน้า 1&2

--- Mainmenu/Mainmenu.cpp
+++ Mainmenu/Mainmenu.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+string namegame = "pokemonkings";
+
+void home(){
+    string entertocontinue;
+    cout << "----------------------------------" << endl;
+    cout << "|                                |" << endl;
+    cout << "|                                |" << endl;
+    cout << "|          pokemonkings          |" << endl;
+    cout << "|                                |" << endl;
+    cout << "|                                |" << endl;
+    cout << "----------------------------------" << endl;
+    cout << "     Please Enter to continue     " << endl;
+    getline(cin, entertocontinue);
+}
 
 void Menu(){
     char who;
 
+        cout << "----------------------------------------" << endl;
+        cout << "(1) Play with Computer (PVE) : Type (C)" << endl;
+        cout << "(2) Play with Player   (PVP) : Type (P)" << endl;
+    
     do{
-        cout << "-------------------" << endl;
-        cout << "Play with Computer : Type (C)" << endl;
-        cout << "Play with Player : Type (P)" << endl;
         cout << "Enter your answer : ";
         cin >> who;
-        cout << "-------------------" << endl;
     }while(who != 'C' && who != 'P');
+
+    if(who == 'C'){
+        cout << ">>> (1) Play with Computer (PVE) <<<" << endl;
+    }    
+    else{
+        cout << ">>> (2)  Play with Player  (PVP) <<<" << endl;
+    }
+    cout << "----------------------------------------" << endl;
 }
 
 int main(){
-    
+    home();
     Menu();
     return 0;
 }
