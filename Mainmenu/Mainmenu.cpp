625fb13b24b6b87d38bf7d6246d101a0bf9c2646
Nice <niceziio08@gmail.com>
2025-01-28 20:15:29 +0700

    หน้า 1-3

--- Mainmenu/Mainmenu.cpp
+++ Mainmenu/Mainmenu.cpp
@@ -3,9 +3,24 @@
 
 using namespace std;
 
-string namegame = "pokemonkings";
+string nameplayers[2];
 
-void home(){
+void Page1();
+char Page2();
+void createwindowCom();
+void createwindowPlayer();
+void Page3(char ComOrPlayer);
+
+
+
+int main(){
+    Page1();
+    char who = Page2();
+    Page3(who);
+    return 0;
+}
+
+void Page1(){
     string entertocontinue;
     cout << "----------------------------------" << endl;
     cout << "|                                |" << endl;
@@ -18,7 +33,7 @@ void home(){
     getline(cin, entertocontinue);
 }
 
-void Menu(){
+char Page2(){
     char who;
 
         cout << "----------------------------------------" << endl;
@@ -36,11 +51,31 @@ void Menu(){
     else{
         cout << ">>> (2)  Play with Player  (PVP) <<<" << endl;
     }
+
     cout << "----------------------------------------" << endl;
+
+    cin.ignore();
+    return who;
 }
 
-int main(){
-    home();
-    Menu();
-    return 0;
+void createwindowCom(){
+    cout << "PLEASE ENTER YOUR NAME : ";
+    getline(cin, nameplayers[0]);
+}
+
+void createwindowPlayer(){
+    cout << "PLEASE ENTER PLAYER 1 NAME : "; 
+    getline(cin, nameplayers[0]);
+    cout << "PLEASE ENTER PLAYER 2 NAME : "; 
+    getline(cin, nameplayers[1]);
+
+}
+
+void Page3(char ComOrPlayer){
+    if(ComOrPlayer == 'C'){
+        createwindowCom();
+    }
+    else{
+        createwindowPlayer();
+    }
 }
