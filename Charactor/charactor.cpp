22a1f3beeb6ec9e5be99a251a36eec61e8bf9166
Nice <niceziio08@gmail.com>
2025-02-04 21:48:58 +0700

    รอใส่ skill ติดตัว

--- Charactor/charactor.cpp
+++ Charactor/charactor.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-string namepokemon[] = {"A", "B", "C", "D", "E"};
+string namepokemon[] = {"Arthur Morgan", "Boudica", "Caesar", "Darius", "Edward the Black Prince", "Frederick the Great"};
 string elementpokemon[] = {"Earth", "Water", "Wind", "Fire"};
 
-struct {                        //สร้างข้อมูลส่วนตัวของโปเกม่อน
+struct personal{                //สร้างข้อมูลส่วนตัวของโปเกม่อน
     string name;                //ชื่อ
     double HP = 100.0;          //เลือด
     double mana = 100.0;        //มานา
-    double armor = 100.0;       //เกราะ
+    double def = 100.0;         //เกราะ
     string element;             //ธาตุ
-}pokemon[5];
+}pokemon[6];
 
 int randomnumber(int n){        //ฟังชั่นสุ่ม
     return rand() % n;
@@ -20,7 +20,7 @@ int randomnumber(int n){        //ฟังชั่นสุ่ม
 
 void createpokemon(){
     srand(time(0));
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < 6; i++){
         int randomelement = (randomnumber(4) * 7) % 4;
         pokemon[i].name = namepokemon[i];
         pokemon[i].element = elementpokemon[randomelement];
@@ -30,8 +30,12 @@ void createpokemon(){
 int main(){
     createpokemon();
     
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < 6; i++){
         cout << pokemon[i].name << " : " << pokemon[i].element << endl;
+        cout << "HP : " << pokemon[i].HP << endl;
+        cout << "Mana : " << pokemon[i].mana << endl;
+        cout << "DEF : " << pokemon[i].def << endl;
+        cout << "-------------------------" << endl;
     }
 
     return 0;
