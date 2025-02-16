b420ad0b2365623a11980737b883b589a61b176f
Phemenon <145136588+Phemenon@users.noreply.github.com>
2025-02-16 23:28:16 +0700

    Update homepage.cpp

--- Demo3/homepage.cpp
+++ Demo3/homepage.cpp
@@ -16,25 +16,20 @@ int main()
         showpokemon();
         p1.choosepokemon(1);
 
-        if(toupper(who) == 'C'){
-            p2.comchoosepokemon();
-        }
-        else{
-            p2.createplayer("p2");
-            showpokemon();
-            p2.choosepokemon(2);
-        }
 
         if(toupper(who) == 'C'){
+            p2.comchoosepokemon();
             while(true)
             {
             /*cout << "Player 1" << "\t\t" << "Player 2" << endl;
             cout << p1 << " HP: " << player[0].pokemon[player[0].pokeplayer].hp << "\t\t";
             cout << player[1].pokemon[player[1].pokeplayer].name << " HP: " << player[1].pokemon[player[1].pokeplayer].hp << "\n";*/
-                delaySeconds(1);
-                p1.showstat(0, 1);
-                p2.showstat(0, 2);
+                
                 while(true){
+                    delaySeconds(1);
+                    p1.showstat(0, 1);
+                    p2.showstat(0, 2);
+
                     cout << "[1] Attack [2] Strike [3] Defend [4] counter" << endl;
                     cout << "Player'1 Choice : ";
                     cin >> p1.playerAction;
@@ -73,6 +68,106 @@ int main()
                     break;
                 }
             } 
+            delaySeconds(2);
+            Return2Menu();
+
+        }else if(toupper(who) == 'P'){
+            p2.createplayer("p2");
+            showpokemon();
+            p2.choosepokemon(2);
+
+           
+            while(true){
+                delaySeconds(1);
+                p1.showstat(0, 1);
+                p2.showstat(0, 2);
+                cout << "----------------------------------" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|         player1' turn_         |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "----------------------------------" << endl;
+                while(true){
+                    cout << "[1] Attack [2] Strike [3] Defend [4] counter" << endl;
+                    cout << "Player'1 Choice : ";
+                    cin >> p1.playerAction;
+                    if (cin.fail() || p1.playerAction < 1 || p1.playerAction > 4) {
+                        cin.clear(); 
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+                        cout << "Invalid input. Please choose again." << endl;
+                    } else {
+                        break;
+                    }
+                }
+                    int action1 = p1.playerAction;
+                    delaySeconds(0.5);
+                    
+                cout << "----------------------------------" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|         player2' turn_         |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "|                                |" << endl;
+                cout << "----------------------------------" << endl;
+                while(true){
+                    cout << "[1] Attack [2] Strike [3] Defend [4] counter" << endl;
+                    cout << "Player'2 Choice : ";
+                    cin >> p2.playerAction;
+                    if (cin.fail() || p2.playerAction < 1 || p2.playerAction > 4) {
+                        cin.clear(); 
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+                        cout << "Invalid input. Please choose again." << endl;
+                    } else {
+                        break;
+                    }
+                }
+                int action2 = p2.playerAction;
+                
+                cout << "===========================================================================================================" << endl;
+                drawscene(action1, p1, action2, p2);
+                cout << "===========================================================================================================" << endl;
+                cout << endl;
+                
+                delaySeconds(1);
+                if(p1.isDead(0) && p2.isDead(0)){
+                    if(p1.checkspd(0) < p2.checkspd(0)){
+                        win(2);
+                        break;
+                    }else if(p1.checkspd(0) == p2.checkspd(0)){
+                        cout << "===========================================================================" << endl;
+                        for(int i = 0; i < 3; i++) cout << endl;
+                        cout << "                                      draw" << endl;
+                        for(int i = 0; i < 3; i++) cout << endl;
+                        cout << "===========================================================================" << endl;
+                        break;
+                    }
+                }
+                else if(p2.isDead(0)){
+                    win(1);
+                    break;
+                }
+                else if(p1.isDead(0)){
+                    win(2);
+                    break;
+                }
+            }
             delaySeconds(2);
             Return2Menu();
         }
